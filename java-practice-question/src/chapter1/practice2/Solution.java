a704d6f3fe80365b732da853267b5e7bc4097afd
sejoung <sejoung@gmail.com>
2016-01-05 11:03:14 +0900

    연습문제 1.2 완료

---
+++ java-practice-question/src/chapter1/practice2/Solution.java
@@ -0,0 +1,11 @@
+package chapter1.practice2;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		System.out.println(5+4+(2-(3-(6+4/5)))/3*((6-2)*(2-7)));
+		//-11
+	}
+
+}
